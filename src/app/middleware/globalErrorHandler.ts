96d6d9e5759071547a035e7990596b562c268ee2
joykbiswas <joykumarbiswas100@gmail.com>
2026-02-21 22:12:18 +0600

    authorization , token set, jwt validation complete. v-1.5

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,25 +1,95 @@
-import status from "http-status";
-import { envVars } from "./../../config/env";
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
+import status from "http-status";
+import z from "zod";
+import AppError from "../errorHelpers/AppError";
+import { envVars } from "../../config/env";
+import { handleZodError } from "../errorHelpers/handleZodError";
+// import { handleZodError } from "../errorHelpers/handleZodError";
+// import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 
-export const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (envVars.NODE_ENV === "development") {
-    console.log("Error from Global Handler", err);
-  }
+export interface TErrorSources {
+    path: string;
+    message: string;
+}
 
-  const statusCode: number = status.INTERNAL_SERVER_ERROR;
-  const message: string = "Internal Server Error";
+export interface TErrorResponse {
+    statusCode?: number;
+    success: boolean;
+    message: string;
+    errorSources: TErrorSources[];
+    stack?: string;
+    error?: unknown;
+}
 
-  res.status(500).json({
-    success: false,
-    message: "Internal Server Error",
-    error: err.message,
-  });
-};
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (envVars.NODE_ENV === 'development') {
+        console.log("Error from Global Error Handler", err);
+    }
+
+    let errorSources: TErrorSources[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = 'Internal Server Error';
+    let stack: string | undefined = undefined;
+
+    //Zod Error Pattern
+    /*
+     error.issues; 
+    /* [
+      {
+        expected: 'string',
+        code: 'invalid_type',
+        path: [ 'username' , 'password' ], => username password
+        message: 'Invalid input: expected string'
+      },
+      {
+        expected: 'number',
+        code: 'invalid_type',
+        path: [ 'xp' ],
+        message: 'Invalid input: expected number'
+      }
+    ] 
+    */
+
+    if (err instanceof z.ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
+        stack = err.stack;
+
+    } else if (err instanceof AppError) {
+        statusCode = err.statusCode;
+        message = err.message;
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+    else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message
+        stack = err.stack;
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+
+
+    const errorResponse: TErrorResponse = {
+        success: false,
+        message: message,
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined,
+        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
+}
--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -1,38 +1,66 @@
 import { Request, Response } from "express";
+import status from "http-status";
+import ms, { StringValue } from "ms";
 import { catchAsync } from "../../shared/catchAsync";
 import { sendResponse } from "../../shared/sendResponse";
+import { tokenUtils } from "../../utils/token";
 import { AuthService } from "./auth.service";
-import status from "http-status";
+import { envVars } from "../../../config/env";
 
 const registerPatient = catchAsync(
     async (req: Request, res: Response) => {
+        const maxAge = ms(envVars.ACCESS_TOKEN_EXPIRES_IN as StringValue);
+        console.log({ maxAge });
         const payload = req.body;
 
+        console.log(payload);
+
         const result = await AuthService.registerPatient(payload);
+
+        const { accessToken, refreshToken, token, ...rest } = result
+
+        tokenUtils.setAccessTokenCookie(res, accessToken);
+        tokenUtils.setRefreshTokenCookie(res, refreshToken);
+        tokenUtils.setBetterAuthSessionCookie(res, token as string);
 
         sendResponse(res, {
             httpStatusCode: status.CREATED,
             success: true,
             message: "Patient registered successfully",
-            data: result,
+            data: {
+                token,
+                accessToken,
+                refreshToken,
+                ...rest,
+            }
         })
     }
 )
-
 
 const loginUser = catchAsync(
     async (req: Request, res: Response) => {
         const payload = req.body;
         const result = await AuthService.loginUser(payload);
+        const { accessToken, refreshToken, token, ...rest } = result
+
+        tokenUtils.setAccessTokenCookie(res, accessToken);
+        tokenUtils.setRefreshTokenCookie(res, refreshToken);
+        tokenUtils.setBetterAuthSessionCookie(res, token);
+
         sendResponse(res, {
             httpStatusCode: status.OK,
             success: true,
             message: "User logged in successfully",
-            data: result,
+            data: {
+                token,
+                accessToken,
+                refreshToken,
+                ...rest,
+
+            },
         })
     }
 )
-
 
 export const AuthController = {
     registerPatient,
